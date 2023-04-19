7b6f5cfee330718507aea871c02c5100605ca160
Samuel <jesuval420@gmail.com>
2023-04-18 23:04:44 -0700

    commit inicial

---
+++ proyecto_U2/caminos_ancho.py
@@ -0,0 +1,50 @@
+import random
+
+
+def GoalTest(lista):
+    return 
+
+def B_Ancho(Frontera):
+    if not Frontera:
+        print("No hay solucion")
+        return
+    EA = Frontera.pop(0)
+    
+    #visitados.append(EA)
+    if GoalTest(EA):
+        print(EA, " es solución")
+        #print("--- %s seconds ---" % (time.time() - start_time))
+        
+        return
+    else:
+        OS = Expand(EA)
+        Frontera += OS
+    B_Ancho(Frontera)
+
+def Expand(EA):
+    V = []
+    for i in range(len(EA)):
+        listAux = list(EA)
+        posicion = listAux[i]
+        if (posicion < len(EA)-1):
+            posicion += 1
+            listAux[i] = posicion
+            #if(listAux not in visitados):
+                #V.append(listAux)
+                
+    return V
+
+ancho_tablero=10
+largo_tablero=10
+porcentaje_bloqueo=15
+tablero = [[0]*ancho_tablero for i in range (largo_tablero)]
+bloqueos = int (ancho_tablero * largo_tablero * porcentaje_bloqueo / 100)
+for k in range(bloqueos):
+    i = random.randint(0, largo_tablero-1)  # fila aleatoria
+    j = random.randint(0, ancho_tablero-1)  # columna aleatoria
+    while tablero[i][j] == 1:  # si el espacio ya fue cambiado, elegir otro aleatorio
+        i = random.randint(0, largo_tablero-1)
+        j = random.randint(0, ancho_tablero-1)
+    tablero[i][j] = 1
+
+
---
+++ proyecto_U2/interfazcaminos.py
@@ -0,0 +1,30 @@
+import tkinter as tk
+
+# Configuración del tablero
+N = 8
+M = 8
+WIDTH = 50
+HEIGHT = 50
+
+# Coordenadas de las casillas a dibujar en otro color
+coords = [(2, 3), (4, 5), (6, 7)]
+
+# Creación de la ventana y del lienzo (canvas)
+root = tk.Tk()
+canvas = tk.Canvas(root, width=N*WIDTH, height=M*HEIGHT)
+canvas.pack()
+
+# Dibujo de las casillas del tablero
+for i in range(N):
+    for j in range(M):
+        x0 = i*WIDTH
+        y0 = j*HEIGHT
+        x1 = x0 + WIDTH
+        y1 = y0 + HEIGHT
+        if (i, j) in coords:
+            canvas.create_rectangle(x0, y0, x1, y1, fill="red")
+        else:
+            canvas.create_rectangle(x0, y0, x1, y1, fill="white")
+
+# Inicio del bucle principal de la aplicación
+root.mainloop()
